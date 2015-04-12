6933a06864cf90f8be28d9ab8acc8f07d484b6ce
Anna Wang <anna.why45@gmail.com>
2015-04-12 12:52:25 -0400

    change the format of RoomRequest

--- WaterChat/RoomRequest.swift
+++ WaterChat/RoomRequest.swift
@@ -39,17 +39,20 @@ class RoomRequest: Message {
     
     var curNum: UInt8 = 0
     var maxNum: UInt8 = 0
-    var nameLen: Int32 = 0
-    var groupName: String
     var name: String
     var groupID: UInt32 = 0
     var members = [MCPeerID]()
     
     override init() {
         var roomInfo = Room()
-        self.groupName = roomInfo.name
-        self.name = self.groupName.substringWithRange(Range<String.Index>(start: self.groupName.startIndex, end: advance(self.groupName.startIndex, 8)))
-        self.nameLen = roomInfo.nameLen
+        var nameLen: Int32 = roomInfo.nameLen
+        var groupName: String = roomInfo.name
+        // To limit that the length of group name is 8 bytes
+        self.name = groupName.substringToIndex(advance(groupName.startIndex, 8))
+        var group = [UInt8](self.name.utf8)
+        println("8 bytes group name \(self.name)")
+        println("encode group name \(group)")
+        
         self.curNum = 1
         self.maxNum = 0
         self.groupID = roomInfo.groupID
@@ -57,31 +60,24 @@ class RoomRequest: Message {
         super.init()
     }
     
+    // Get the Room Request message to initialize the Room() structure
     override init(bytes data: NSData) {
         
         var roomInfo = Room()
         
-        self.groupName = roomInfo.name
-        self.nameLen = roomInfo.nameLen
-        
-        // To limit that the length of group name is 8 bytes
-        self.name = self.groupName.substringToIndex(advance(self.groupName.startIndex, 8))
-        var group = [UInt8](self.name.utf8)
-        println("8 bytes group name \(self.name)")
-        println("encode group name \(group)")
-        
-        self.curNum = roomInfo.currentNumber
-        self.maxNum = roomInfo.maximumNumber
-        self.groupID = roomInfo.groupID
         self.members = roomInfo.groupMemberList
         
         // The first 8 bytes are type of RoomRequest
         data.getBytes(&self.curNum, range: NSMakeRange(2, 1))
         data.getBytes(&self.maxNum, range: NSMakeRange(3, 1))
         data.getBytes(&self.groupID, range: NSMakeRange(4, 4))
-        //data.getBytes(&self.nameLen, range: NSMakeRange(8, 4))
         
-        var content: [UInt8] = Util.toByteArray(data) + Util.toByteArray(self.name)
+        var nameBytes = [UInt8](count: SIZE, repeatedValue: 0)
+        data.getBytes(&nameBytes, range: NSMakeRange(8, 8))
+        self.name = NSString(bytes: nameBytes,length: 8, encoding: NSUTF8StringEncoding)!
+        
+        var content:[Byte] = Util.toByteArray(data)
+        println("The content of the received message is: \(content)")
         for member in members {
             content += Util.toByteArray(member)
         }
@@ -90,6 +86,7 @@ class RoomRequest: Message {
         super.init(bytes: nsData)
     }
     
+    // Send out the room request message
     override func serialize() -> NSData {
         var array = [UInt8](count: SIZE, repeatedValue: 0)
         array[0] = MessageType.ROOMREQ.rawValue
@@ -101,13 +98,10 @@ class RoomRequest: Message {
             array[i] = namepart
             i += 1
         }
-        
         var uint32P =  UnsafeMutablePointer<UInt32>(array)
         uint32P[1] = self.groupID
         
         var nsData = NSData(bytes: array, length: SIZE)
-        let str = NSString(data: nsData, encoding: NSUTF8StringEncoding)
-        
         return nsData
     }
 }
