add013378969331532f4a62addff2a0890ab7056
LollipopLollipop <jodie.d.zhao@gmail.com>
2015-04-24 09:29:49 -0400

    add jsqmsg

---
+++ WaterChat-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WaterChat-Bridging-Header.h
+//  WaterChat
+//
+//  Created by Ding ZHAO on 4/23/15.
+//  Copyright (c) 2015 Hsueh-Hung Cheng. All rights reserved.
+//
+
+#ifndef WaterChat_WaterChat_Bridging_Header_h
+#define WaterChat_WaterChat_Bridging_Header_h
+
+#import "JSQMessagesViewController/JSQMessagesViewController/JSQMessages.h"
+#endif
---
+++ WaterChat/MessageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MessageViewController.swift
+//  WaterChat
+//
+//  Created by Ding ZHAO on 4/23/15.
+//  Copyright (c) 2015 Hsueh-Hung Cheng. All rights reserved.
+//
+
+import Cocoa
+
+class MessageViewController: UIViewController {
+
+}
