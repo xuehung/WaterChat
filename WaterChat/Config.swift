1e9164cbd9374fb2251a6bc15756e9f1f3a28933
xuehung <xuehung@gmail.com>
2015-04-29 22:12:09 -0400

    chage config.macaddr

--- WaterChat/Config.swift
+++ WaterChat/Config.swift
@@ -11,6 +11,12 @@ import Foundation
 // for configuration
 struct Config {
     static let serviceType = "WaterChat"
-    static let address: MacAddr = 99999
-    static let maxNeighbor = 2
+    static let address: MacAddr = {
+        var udid = UIDevice.currentDevice().identifierForVendor.UUIDString
+        var length = count(udid)
+        let range = Range(start: (advance(udid.endIndex, -12)), end: udid.endIndex)
+        let macAddress = udid.substringWithRange(range)
+        return Util.convertDisplayNameToMacAddr(macAddress)
+    }()
+    static let maxNeighbor = 3
 }
